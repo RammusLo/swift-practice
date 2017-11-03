e7ce74f00d97dc3d5608e46122c8abd80466bd75
Rammus Lo <rammus26@gmail.com>
2017-11-03 16:37:32 -0700

    Initial commit

---
+++ swift practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift practice
+//
+//  Created by UCode on 11/3/17.
+//  Copyright © 2017 Rammus Lo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
